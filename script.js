025159cbc8a0a1e518d7d735e0a47bf843e4e966
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-06-21 11:42:47 -0500

    set up leaflet and javascript geolocator api

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict"
+
+
+navigator.geolocation.getCurrentPosition(
+    function(position) {
+        const {latitude} = position.coords
+        const {longitude} = position.coords
+    }, function() {
+        console.log("none")
+    }
+);
