428bc8e47832f40fa37c62ef6c069bbb5b3d3d25
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-06-21 15:54:36 -0500

    Setup HTML

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 "use strict"
 
+// navigator.geolocation.getCurrentPosition(
+//     function(position) {
+//         const {latitude} = position.coords
+//         const {longitude} = position.coords
+//         const coordinates = [latitude, longitude]
+//         console.log(coordinates)
+//         const map = L.map('map').setView(coordinates, 15);
+        
+//         L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+//             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+//         }).addTo(map);
 
-navigator.geolocation.getCurrentPosition(
-    function(position) {
-        const {latitude} = position.coords
-        const {longitude} = position.coords
-    }, function() {
-        console.log("none")
-    }
-);
+//         map.on('click', function(mapEvent) {
+//             const {lat} = mapEvent.latlng
+//             const {lng} = mapEvent.latlng
+//             const markerClickCoordinates = [lat, lng]
+//             L.marker(markerClickCoordinates).addTo(map)
+//             .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//             .openPopup();
+//         })
+//     }, 
+//     function() {
+//         console.log("none")
+//     }
+// );
