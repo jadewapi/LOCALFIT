6f9ae361d558d0bce3c06eeb201f4cba3a39551d
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-06-21 20:24:19 -0500

    display click on map

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-"use strict"
+"use strict";
 
 navigator.geolocation.getCurrentPosition(
-    function(position) {
-        const {latitude} = position.coords
-        const {longitude} = position.coords
-        const coordinates = [latitude, longitude]
-        console.log(coordinates)
-        const map = L.map('map').setView(coordinates, 15);
-        
-        L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-        }).addTo(map);
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coordinates = [latitude, longitude];
+    console.log(coordinates);
+    const map = L.map("map").setView(coordinates, 15);
 
-        L.marker(coordinates).addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+    L.tileLayer(
+      "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      }
+    ).addTo(map);
 
-    }, 
-    function() {
-        console.log("none")
-    }
+    L.marker(coordinates)
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
+
+    map.on("click", function (mapEvent) {
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+          }).openPopup()
+        );
+    });
+  },
+  function () {
+    console.log("none");
+  }
 );
