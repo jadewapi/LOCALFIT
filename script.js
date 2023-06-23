cf30736db2c22a507217521c3269da7ba317a923
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-06-22 19:47:37 -0500

    current state

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 "use strict";
 
+const logWorkout = document.querySelector(".logWorkout");
+const durationInput = document.querySelector(".durationInput");
+const distanceInput = document.querySelector(".distanceInput");
+const elevationGainInput = document.querySelector(".elevationGainInput");
+const cadenceInput = document.querySelector(".cadenceInput");
+const displayedSummary = document.querySelector(".displayedSummary");
+
+displayedSummary.style.display = "none";
+logWorkout.style.display = "none";
+
+let map, mapEvent;
+
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     const coordinates = [latitude, longitude];
     console.log(coordinates);
-    const map = L.map("map").setView(coordinates, 15);
-
+    map = L.map("map").setView(coordinates, 11);
     L.tileLayer(
       "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
       {
@@ -16,26 +27,39 @@ navigator.geolocation.getCurrentPosition(
       }
     ).addTo(map);
 
-    L.marker(coordinates)
-      .addTo(map)
-      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-      .openPopup();
-
-    map.on("click", function (mapEvent) {
-      const { lat, lng } = mapEvent.latlng;
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-          }).openPopup()
-        );
+    map.on("click", function (mapE) {
+      mapEvent = mapE;
+      distanceInput.focus();
+      logWorkout.style.display = "";
     });
   },
   function () {
     console.log("none");
   }
 );
+
+logWorkout.addEventListener("submit", function (e) {
+  console.log("yeeet");
+  e.preventDefault();
+
+  durationInput.value =
+    distanceInput.value =
+    elevationGainInput.value =
+    cadenceInput.value =
+      "";
+
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
