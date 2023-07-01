2ce64346888ad0436186fe83f7e6ec234615e695
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-06-30 19:10:53 -0500

    javascript geolocation api

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coordinates = [latitude, longitude];
+    const map = L.map("map").setView(coordinates, 13);
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png", {
+      attribution:
+        'CycloSM &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+    //
+    L.marker(coordinates)
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.").openPo;
+    pup();
+  },
+  function () {}
+);
