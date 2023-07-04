5cf50b221f0f08610fa0b3dc112aecd27037f4fc
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-07-03 20:33:24 -0500

    OOP copy

--- script.js
+++ script.js
@@ -1,62 +1,131 @@
 "use strict";
+
 const logWorkout = document.querySelector(".logWorkout");
+const selectType = document.querySelector(".select");
+const elevationGainContainer = document.querySelector(
+  ".elevationGainContainer"
+);
+const cadenceContainer = document.querySelector(".cadenceContainer");
+//
 const durationInput = document.querySelector(".durationInput");
 const distanceInput = document.querySelector(".distanceInput");
 const elevationGainInput = document.querySelector(".elevationGainInput");
 const cadenceInput = document.querySelector(".cadenceInput");
-const displayedSummary = document.querySelector(".displayedSummary");
-const specificWorkout = document.querySelectorAll(".specificWorkout");
 
-let map, mapEvent;
+cadenceContainer.style.display = "none";
+elevationGainInput.setAttribute("required", "");
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+class Workout {
+  constructor(duration, distance) {
+    this.duration = duration;
+    this.distance = distance;
+  }
+}
+
+class Running extends Workout {
+  type = "Running";
+  //
+  constructor(duration, distance, elevationGain) {
+    super(duration, distance);
+    this.elevationGain = elevationGain;
+  }
+}
+
+class Cycling extends Workout {
+  type = "Cycling";
+  //
+  constructor(duration, distance, cadence) {
+    super(type, duration, distance);
+    this.cadence = cadence;
+  }
+}
+
+class App {
+  // the map div
+  #map;
+  // a specific coordinate that is an object that is clicked in the map div
+  mapEvent;
+  #inputValues;
+  //
+  constructor() {
+    this._getPosition();
+    selectType.addEventListener(
+      "change",
+      this._toggleElevationField.bind(this)
+    );
+    //
+    logWorkout.addEventListener("input", this._checkInputForNumbers.bind(this));
+    //
+    logWorkout.addEventListener("submit", this._newWorkout);
+  }
+  // uses geolocation APU to get position object.
+  _getPosition() {
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this),
+      function () {
+        alert("Please allow location");
+      }
+    );
+  }
+  // produces the map and adds the user's location
+  _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     const coordinates = [latitude, longitude];
-    console.log(coordinates);
-    map = L.map("map").setView(coordinates, 11);
-    L.tileLayer(
-      "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      }
-    ).addTo(map);
-
-    map.on("click", function (mapE) {
-      mapEvent = mapE;
-      distanceInput.focus();
-      logWorkout.style.display = "";
-    });
-  },
-  function () {
-    console.log("none");
+    //
+    this.#map = L.map("map").setView(coordinates, 13);
+    //
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png", {
+      attribution:
+        'CycloSM &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+    //
+    //
+    this.#map.on("click", this._showForm.bind(this));
   }
-);
+  // shows form for each odd number amount of clicks.
+  _showForm(mapEvent) {
+    this.mapEvent = mapEvent;
+    const { lat } = this.mapEvent.latlng;
+    const { lng } = this.mapEvent.latlng;
+    logWorkout.classList.toggle("hidden");
+  }
+  // toggles the options in the select HTML tag to show either elevation(running) or cadence(cycling)
+  _toggleElevationField() {
+    const selectedOption = selectType.value;
+    //
+    if (selectedOption === "Running") {
+      elevationGainContainer.style.display = "";
+      elevationGainInput.setAttribute("required", "");
+      cadenceInput.removeAttribute("required");
+      cadenceContainer.style.display = "none";
+    } else if (selectedOption === "Cycling") {
+      cadenceContainer.style.display = "";
+      cadenceInput.setAttribute("required", "");
+      elevationGainInput.removeAttribute("required");
+      elevationGainContainer.style.display = "none";
+    }
+  }
+  // validates form input by restricting to only numbers
+  _checkInputForNumbers(inputValues) {
+    this.#inputValues = inputValues;
+    const target = this.#inputValues.target;
+    if (target.tagName === "INPUT") {
+      const currentValue = target.value;
+      const sanitizedValue = currentValue.replace(/[^0-9]/g, "");
+      target.value = sanitizedValue;
+    }
+  }
+  // adds a new workout based on the options selected in the select HTML tag.
+  _newWorkout(e) {
+    e.preventDefault();
+    console.log(this.mapEvent);
+    durationInput.value =
+      distanceInput.value =
+      elevationGainInput.value =
+      cadenceInput.value =
+        "";
+  }
+}
 
-logWorkout.addEventListener("submit", function (e) {
-  console.log("yeeet");
-  e.preventDefault();
-
-  durationInput.value =
-    distanceInput.value =
-    elevationGainInput.value =
-    cadenceInput.value =
-      "";
-
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: "running",
-      })
-    )
-    .setPopupContent("Workout")
-    .openPopup();
-});
+const app = new App();
