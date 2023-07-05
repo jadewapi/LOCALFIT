8340a0abb0deebecf46f99c19ea52ae587f52ef9
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-07-05 18:38:32 -0500

    Adding methods to form validaiton

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ const durationInput = document.querySelector(".durationInput");
 const distanceInput = document.querySelector(".distanceInput");
 const elevationGainInput = document.querySelector(".elevationGainInput");
 const cadenceInput = document.querySelector(".cadenceInput");
+const workoutsContainer = document.querySelector(".workoutsContainer");
+const typeInput = document.querySelector("#typeInput");
 
-cadenceContainer.style.display = "none";
-elevationGainInput.setAttribute("required", "");
+elevationGainContainer.style.display = "none";
+cadenceInput.setAttribute("required", "");
 
 const months = [
   "January",
@@ -53,65 +55,71 @@ class Running extends Workout {
     return (this.pace = this.duration / this.distance);
   }
   HTML() {
-    return `<article class="displayedSummary">
-    <div class="summaryType">${this.type} on ${
+    return `<div class="specificWorkout">
+    <article class="displayedSummary">
+      <div class="summaryType">${this.type} on ${
       months[this.date.getMonth()]
     } ${this.date.getDay()}</div>
-    <div class="summaryDistance">
-      <svg
-        class="svgDistance"
-        xmlns="http://www.w3.org/2000/svg"
-        height="3rem"
-        viewBox="0 0 416 512"
-      >
-        <style>
-          svg {
-            fill: #23508b;
-          }
-        </style>
-        <path
-          d="M272 96c26.51 0 48-21.49 48-48S298.51 0 272 0s-48 21.49-48 48 21.49 48 48 48zM113.69 317.47l-14.8 34.52H32c-17.67 0-32 14.33-32 32s14.33 32 32 32h77.45c19.25 0 36.58-11.44 44.11-29.09l8.79-20.52-10.67-6.3c-17.32-10.23-30.06-25.37-37.99-42.61zM384 223.99h-44.03l-26.06-53.25c-12.5-25.55-35.45-44.23-61.78-50.94l-71.08-21.14c-28.3-6.8-57.77-.55-80.84 17.14l-39.67 30.41c-14.03 10.75-16.69 30.83-5.92 44.86s30.84 16.66 44.86 5.92l39.69-30.41c7.67-5.89 17.44-8 25.27-6.14l14.7 4.37-37.46 87.39c-12.62 29.48-1.31 64.01 26.3 80.31l84.98 50.17-27.47 87.73c-5.28 16.86 4.11 34.81 20.97 40.09 3.19 1 6.41 1.48 9.58 1.48 13.61 0 26.23-8.77 30.52-22.45l31.64-101.06c5.91-20.77-2.89-43.08-21.64-54.39l-61.24-36.14 31.31-78.28 20.27 41.43c8 16.34 24.92 26.89 43.11 26.89H384c17.67 0 32-14.33 32-32s-14.33-31.99-32-31.99z"
-        />
-      </svg>
-      <p>${this.distance} M</p>
-    </div>
-    <div class="summaryDuration">
-      <svg
-        class="svgDuration"
-        xmlns="http://www.w3.org/2000/svg"
-        height="3rem"
-        viewBox="0 0 512 512"
-      >
-        <style>
-          svg {
-            fill: #23508b;
-          }
-        </style>
-        <path
-          d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
-        />
-      </svg>
-      <p>${this.duration} Min</p>
-    </div>
-    <div class="summaryCadence">
-      <svg
-        class="svgCadence"
-        xmlns="http://www.w3.org/2000/svg"
-        height="3rem"
-        viewBox="0 0 640 512"
-      >
-        <style>
-          svg {
-            fill: #23508b;
-          }
-        </style>
-        <path
-          d="M416 0C352.3 0 256 32 256 32V160c48 0 76 16 104 32s56 32 104 32c56.4 0 176-16 176-96S512 0 416 0zM128 96c0 35.3 28.7 64 64 64h32V32H192c-35.3 0-64 28.7-64 64zM288 512c96 0 224-48 224-128s-119.6-96-176-96c-48 0-76 16-104 32s-56 32-104 32V480s96.3 32 160 32zM0 416c0 35.3 28.7 64 64 64H96V352H64c-35.3 0-64 28.7-64 64z"
-        />
-      </svg>
-      <p>${this.cadence} SPM</p>
-    </div>
-  </article>`;
+      <div class="summaryDistance">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="3rem"
+          viewBox="0 0 576 512"
+        >
+          <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+          <path
+            d="M408 120c0 54.6-73.1 151.9-105.2 192c-7.7 9.6-22 9.6-29.6 0C241.1 271.9 168 174.6 168 120C168 53.7 221.7 0 288 0s120 53.7 120 120zm8 80.4c3.5-6.9 6.7-13.8 9.6-20.6c.5-1.2 1-2.5 1.5-3.7l116-46.4C558.9 123.4 576 135 576 152V422.8c0 9.8-6 18.6-15.1 22.3L416 503V200.4zM137.6 138.3c2.4 14.1 7.2 28.3 12.8 41.5c2.9 6.8 6.1 13.7 9.6 20.6V451.8L32.9 502.7C17.1 509 0 497.4 0 480.4V209.6c0-9.8 6-18.6 15.1-22.3l122.6-49zM327.8 332c13.9-17.4 35.7-45.7 56.2-77V504.3L192 449.4V255c20.5 31.3 42.3 59.6 56.2 77c20.5 25.6 59.1 25.6 79.6 0zM288 152a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"
+          />
+        </svg>
+        <p>${this.distance} Miles</p>
+      </div>
+      <div class="summaryDuration">
+        <svg
+          class="svgDuration"
+          xmlns="http://www.w3.org/2000/svg"
+          height="3rem"
+          viewBox="0 0 512 512"
+        >
+          <style>
+            svg {
+              fill: #23508b;
+            }
+          </style>
+          <path
+            d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
+          />
+        </svg>
+        <p>${this.duration} Min</p>
+      </div>
+      <div class="summarySpeed">
+        <svg
+          class="svgSpeed"
+          xmlns="http://www.w3.org/2000/svg"
+          height="3rem"
+          viewBox="0 0 576 512"
+        >
+          <path
+            d="M288 32C128.94 32 0 160.94 0 320c0 52.8 14.25 102.26 39.06 144.8 5.61 9.62 16.3 15.2 27.44 15.2h443c11.14 0 21.83-5.58 27.44-15.2C561.75 422.26 576 372.8 576 320c0-159.06-128.94-288-288-288zm0 64c14.71 0 26.58 10.13 30.32 23.65-1.11 2.26-2.64 4.23-3.45 6.67l-9.22 27.67c-5.13 3.49-10.97 6.01-17.64 6.01-17.67 0-32-14.33-32-32S270.33 96 288 96zM96 384c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm48-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm246.77-72.41l-61.33 184C343.13 347.33 352 364.54 352 384c0 11.72-3.38 22.55-8.88 32H232.88c-5.5-9.45-8.88-20.28-8.88-32 0-33.94 26.5-61.43 59.9-63.59l61.34-184.01c4.17-12.56 17.73-19.45 30.36-15.17 12.57 4.19 19.35 17.79 15.17 30.36zm14.66 57.2l15.52-46.55c3.47-1.29 7.13-2.23 11.05-2.23 17.67 0 32 14.33 32 32s-14.33 32-32 32c-11.38-.01-20.89-6.28-26.57-15.22zM480 384c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"
+          />
+        </svg>
+        <p>${this.pace} MPH</p>
+      </div>
+      <div class="summaryCadence">
+        <svg
+          class="svgCadence"
+          xmlns="http://www.w3.org/2000/svg"
+          height="3.5rem"
+          viewBox="0 0 448 512"
+        >
+          <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+          <path
+            d="M320 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM125.7 175.5c9.9-9.9 23.4-15.5 37.5-15.5c1.9 0 3.8 .1 5.6 .3L137.6 254c-9.3 28 1.7 58.8 26.8 74.5l86.2 53.9-25.4 88.8c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l28.7-100.4c5.9-20.6-2.6-42.6-20.7-53.9L238 299l30.9-82.4 5.1 12.3C289 264.7 323.9 288 362.7 288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H362.7c-12.9 0-24.6-7.8-29.5-19.7l-6.3-15c-14.6-35.1-44.1-61.9-80.5-73.1l-48.7-15c-11.1-3.4-22.7-5.2-34.4-5.2c-31 0-60.8 12.3-82.7 34.3L57.4 153.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l23.1-23.1zM91.2 352H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h69.6c19 0 36.2-11.2 43.9-28.5L157 361.6l-9.5-6c-17.5-10.9-30.5-26.8-37.9-44.9L91.2 352z"
+          />
+        </svg>
+        <p>${this.cadence} SPM</p>
+      </div>
+    </article>
+  </div>`;
   }
   displayText() {
     return `${this.type} on ${
@@ -131,60 +139,69 @@ class Cycling extends Workout {
     return (this.speed = this.distance / (this.duration / 60));
   }
   HTML() {
-    return `<article class="displayedSummary">
-    <div class="summaryType">
-      ${this.type} on ${months[this.date.getMonth()]} ${this.date.getDay()}
-    </div>
-    <div class="summaryDistance">
-      <svg
-        class="svgDistance"
-        xmlns="http://www.w3.org/2000/svg"
-        height="3rem"
-        viewBox="0 0 416 512"
-      >
-        <style>
-          svg {
-            fill: #23508b;
-          }
-        </style>
-        <path
-          d="M272 96c26.51 0 48-21.49 48-48S298.51 0 272 0s-48 21.49-48 48 21.49 48 48 48zM113.69 317.47l-14.8 34.52H32c-17.67 0-32 14.33-32 32s14.33 32 32 32h77.45c19.25 0 36.58-11.44 44.11-29.09l8.79-20.52-10.67-6.3c-17.32-10.23-30.06-25.37-37.99-42.61zM384 223.99h-44.03l-26.06-53.25c-12.5-25.55-35.45-44.23-61.78-50.94l-71.08-21.14c-28.3-6.8-57.77-.55-80.84 17.14l-39.67 30.41c-14.03 10.75-16.69 30.83-5.92 44.86s30.84 16.66 44.86 5.92l39.69-30.41c7.67-5.89 17.44-8 25.27-6.14l14.7 4.37-37.46 87.39c-12.62 29.48-1.31 64.01 26.3 80.31l84.98 50.17-27.47 87.73c-5.28 16.86 4.11 34.81 20.97 40.09 3.19 1 6.41 1.48 9.58 1.48 13.61 0 26.23-8.77 30.52-22.45l31.64-101.06c5.91-20.77-2.89-43.08-21.64-54.39l-61.24-36.14 31.31-78.28 20.27 41.43c8 16.34 24.92 26.89 43.11 26.89H384c17.67 0 32-14.33 32-32s-14.33-31.99-32-31.99z"
-        />
-      </svg>
-      <p>${this.distance} M</p>
-    </div>
-    <div class="summaryDuration">
-      <svg
-        class="svgDuration"
-        xmlns="http://www.w3.org/2000/svg"
-        height="3rem"
-        viewBox="0 0 512 512"
-      >
-        <style>
-          svg {
-            fill: #23508b;
-          }
-        </style>
-        <path
-          d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
-        />
-      </svg>
-      <p>${this.duration} Min</p>
-    </div>
-    <div class="summarySpeed">
-      <svg
-        class="svgSpeed"
-        xmlns="http://www.w3.org/2000/svg"
-        height="3rem"
-        viewBox="0 0 576 512"
-      >
-        <path
-          d="M288 32C128.94 32 0 160.94 0 320c0 52.8 14.25 102.26 39.06 144.8 5.61 9.62 16.3 15.2 27.44 15.2h443c11.14 0 21.83-5.58 27.44-15.2C561.75 422.26 576 372.8 576 320c0-159.06-128.94-288-288-288zm0 64c14.71 0 26.58 10.13 30.32 23.65-1.11 2.26-2.64 4.23-3.45 6.67l-9.22 27.67c-5.13 3.49-10.97 6.01-17.64 6.01-17.67 0-32-14.33-32-32S270.33 96 288 96zM96 384c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm48-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm246.77-72.41l-61.33 184C343.13 347.33 352 364.54 352 384c0 11.72-3.38 22.55-8.88 32H232.88c-5.5-9.45-8.88-20.28-8.88-32 0-33.94 26.5-61.43 59.9-63.59l61.34-184.01c4.17-12.56 17.73-19.45 30.36-15.17 12.57 4.19 19.35 17.79 15.17 30.36zm14.66 57.2l15.52-46.55c3.47-1.29 7.13-2.23 11.05-2.23 17.67 0 32 14.33 32 32s-14.33 32-32 32c-11.38-.01-20.89-6.28-26.57-15.22zM480 384c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"
-        />
-      </svg>
-      <p>${this.speed} MPH</p>
-    </div>
-    </article>`;
+    return `<div class="specificWorkout">
+    <article class="displayedSummary">
+      <div class="summaryType">${this.type} on  ${
+      months[this.date.getMonth()]
+    } ${this.date.getDay()}</div>
+      <div class="summaryDistance">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="3rem"
+          viewBox="0 0 576 512"
+        >
+          <path
+            d="M408 120c0 54.6-73.1 151.9-105.2 192c-7.7 9.6-22 9.6-29.6 0C241.1 271.9 168 174.6 168 120C168 53.7 221.7 0 288 0s120 53.7 120 120zm8 80.4c3.5-6.9 6.7-13.8 9.6-20.6c.5-1.2 1-2.5 1.5-3.7l116-46.4C558.9 123.4 576 135 576 152V422.8c0 9.8-6 18.6-15.1 22.3L416 503V200.4zM137.6 138.3c2.4 14.1 7.2 28.3 12.8 41.5c2.9 6.8 6.1 13.7 9.6 20.6V451.8L32.9 502.7C17.1 509 0 497.4 0 480.4V209.6c0-9.8 6-18.6 15.1-22.3l122.6-49zM327.8 332c13.9-17.4 35.7-45.7 56.2-77V504.3L192 449.4V255c20.5 31.3 42.3 59.6 56.2 77c20.5 25.6 59.1 25.6 79.6 0zM288 152a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"
+          />
+        </svg>
+        <p>${this.distance} Miles</p>
+      </div>
+      <div class="summaryDuration">
+        <svg
+          class="svgDuration"
+          xmlns="http://www.w3.org/2000/svg"
+          height="3rem"
+          viewBox="0 0 512 512"
+        >
+          <style>
+            svg {
+              fill: #23508b;
+            }
+          </style>
+          <path
+            d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
+          />
+        </svg>
+        <p>${this.duration} Min</p>
+      </div>
+      <div class="summarySpeed">
+        <svg
+          class="svgSpeed"
+          xmlns="http://www.w3.org/2000/svg"
+          height="3rem"
+          viewBox="0 0 576 512"
+        >
+          <path
+            d="M288 32C128.94 32 0 160.94 0 320c0 52.8 14.25 102.26 39.06 144.8 5.61 9.62 16.3 15.2 27.44 15.2h443c11.14 0 21.83-5.58 27.44-15.2C561.75 422.26 576 372.8 576 320c0-159.06-128.94-288-288-288zm0 64c14.71 0 26.58 10.13 30.32 23.65-1.11 2.26-2.64 4.23-3.45 6.67l-9.22 27.67c-5.13 3.49-10.97 6.01-17.64 6.01-17.67 0-32-14.33-32-32S270.33 96 288 96zM96 384c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm48-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm246.77-72.41l-61.33 184C343.13 347.33 352 364.54 352 384c0 11.72-3.38 22.55-8.88 32H232.88c-5.5-9.45-8.88-20.28-8.88-32 0-33.94 26.5-61.43 59.9-63.59l61.34-184.01c4.17-12.56 17.73-19.45 30.36-15.17 12.57 4.19 19.35 17.79 15.17 30.36zm14.66 57.2l15.52-46.55c3.47-1.29 7.13-2.23 11.05-2.23 17.67 0 32 14.33 32 32s-14.33 32-32 32c-11.38-.01-20.89-6.28-26.57-15.22zM480 384c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"
+          />
+        </svg>
+        <p>${this.speed} MPH</p>
+      </div>
+      <div class="summaryElevationGain">
+        <svg
+          class="svgElevationGain"
+          xmlns="http://www.w3.org/2000/svg"
+          height="3rem"
+          viewBox="0 0 640 512"
+        >
+          <path
+            d="M400 96a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm27.2 64l-61.8-48.8c-17.3-13.6-41.7-13.8-59.1-.3l-83.1 64.2c-30.7 23.8-28.5 70.8 4.3 91.6L288 305.1V416c0 17.7 14.3 32 32 32s32-14.3 32-32V288c0-10.7-5.3-20.7-14.2-26.6L295 232.9l60.3-48.5L396 217c5.7 4.5 12.7 7 20 7h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H427.2zM56 384a72 72 0 1 1 144 0A72 72 0 1 1 56 384zm200 0A128 128 0 1 0 0 384a128 128 0 1 0 256 0zm184 0a72 72 0 1 1 144 0 72 72 0 1 1 -144 0zm200 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"
+          />
+        </svg>
+        <p>${this.elevationGain} SPM</p>
+      </div>
+    </article>
+  </div>`;
   }
   displayText() {
     return `${this.type} on ${
@@ -198,6 +215,7 @@ class Cycling extends Workout {
 class App {
   // the map div
   #map;
+  #mapZoom = 13;
   // a specific coordinate that is an object that is clicked in the map div
   #mapEvent;
   #inputValues;
@@ -210,8 +228,6 @@ class App {
       "change",
       this._toggleElevationField.bind(this)
     );
-    //
-    logWorkout.addEventListener("input", this._checkInputForNumbers.bind(this));
     //
     logWorkout.addEventListener("submit", this._newWorkout.bind(this));
   }
@@ -230,7 +246,7 @@ class App {
     const { longitude } = position.coords;
     const coordinates = [latitude, longitude];
     //
-    this.#map = L.map("map").setView(coordinates, 13);
+    this.#map = L.map("map").setView(coordinates, this.#mapZoom);
     //
     L.tileLayer("https://{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png", {
       attribution:
@@ -251,63 +267,69 @@ class App {
   // toggles the options in the select HTML tag to show either elevation(running) or cadence(cycling)
   _toggleElevationField() {
     //
-    if (selectType.value === "Running") {
+    if (selectType.value === "Cycling") {
       elevationGainContainer.style.display = "";
       elevationGainInput.setAttribute("required", "");
       cadenceInput.removeAttribute("required");
       cadenceContainer.style.display = "none";
-    } else if (selectType.value === "Cycling") {
+    } else if (selectType.value === "Running") {
       cadenceContainer.style.display = "";
       cadenceInput.setAttribute("required", "");
       elevationGainInput.removeAttribute("required");
       elevationGainContainer.style.display = "none";
     }
   }
-  // validates form input by restricting to only numbers
-  _checkInputForNumbers(inputValues) {
-    this.#inputValues = inputValues;
-    const target = this.#inputValues.target;
-    if (target.tagName === "INPUT") {
-      const currentValue = target.value;
-      let sanitizedValue = currentValue.replace(/[^0-9]/g, "");
-      sanitizedValue = Math.min(parseInt(sanitizedValue, 10), 99);
-      target.value = sanitizedValue;
-    }
-  }
   // adds a new workout based on the options selected in the select HTML tag.
   _newWorkout(e) {
     e.preventDefault();
-    //
     let workout;
     const { lat } = this.#mapEvent.latlng;
     const { lng } = this.#mapEvent.latlng;
+    const duration = parseFloat(durationInput.value).toFixed(2);
+    const distance = parseFloat(distanceInput.value).toFixed(2);
+    const typeNum = parseFloat(typeInput.value).toFixed(2);
     //
-    if (selectType.value === "Running") {
-      workout = new Running(
-        [lat, lng],
-        Number(durationInput.value),
-        Number(distanceInput.value),
-        Number(cadenceInput.value)
-      );
-    } else if (selectType.value === "Cycling") {
-      workout = new Cycling(
-        [lat, lng],
-        Number(durationInput.value),
-        Number(distanceInput.value),
-        Number(elevationGainInput.value)
-      );
-    }
+    workout = this._instantiateWorkoutType(
+      selectType.value,
+      duration,
+      distance,
+      typeNum,
+      lat,
+      lng
+    );
+    //
     this.#allWorkouts.push(workout);
-    console.log(this.#allWorkouts);
-    //
+    this._addOnHTMLWorkouts(workout.HTML());
     this._clearInputValues();
   }
+  //clears the value attribute of the inout tags
   _clearInputValues() {
     durationInput.value =
       distanceInput.value =
       elevationGainInput.value =
       cadenceInput.value =
         "";
+  }
+  //create an instance of either running of cycling
+  _instantiateWorkoutType(type, duration, distance, workoutUnit, lat, lng) {
+    if (type === "Running") {
+      return new Running(
+        [lat, lng],
+        Number(duration),
+        Number(distance),
+        Number(workoutUnit)
+      );
+    } else if (type === "Cycling") {
+      return new Cycling(
+        [lat, lng],
+        Number(duration),
+        Number(distance),
+        Number(workoutUnit)
+      );
+    }
+  }
+  _addOnHTMLWorkouts(html) {
+    workoutsContainer.insertAdjacentHTML("afterbegin", html);
   }
 }
 
