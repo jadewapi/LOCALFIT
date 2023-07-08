e50f66c7e7252465bd0f2ef1b9a4d9a5bb78e6ce
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-07-08 01:01:23 -0500

    fix required error

--- script.js
+++ script.js
@@ -78,6 +78,7 @@ class Running extends Workout {
         >
           <!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
           <path
+            class="ass"
             d="M408 120c0 54.6-73.1 151.9-105.2 192c-7.7 9.6-22 9.6-29.6 0C241.1 271.9 168 174.6 168 120C168 53.7 221.7 0 288 0s120 53.7 120 120zm8 80.4c3.5-6.9 6.7-13.8 9.6-20.6c.5-1.2 1-2.5 1.5-3.7l116-46.4C558.9 123.4 576 135 576 152V422.8c0 9.8-6 18.6-15.1 22.3L416 503V200.4zM137.6 138.3c2.4 14.1 7.2 28.3 12.8 41.5c2.9 6.8 6.1 13.7 9.6 20.6V451.8L32.9 502.7C17.1 509 0 497.4 0 480.4V209.6c0-9.8 6-18.6 15.1-22.3l122.6-49zM327.8 332c13.9-17.4 35.7-45.7 56.2-77V504.3L192 449.4V255c20.5 31.3 42.3 59.6 56.2 77c20.5 25.6 59.1 25.6 79.6 0zM288 152a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"
           />
         </svg>
@@ -247,6 +248,7 @@ class App {
     logWorkout.addEventListener("input", this._checkInputValues);
     workoutsContainer.addEventListener("click", function (e) {
       console.log(e.target.closest(".delete"));
+      console.log(e.target.closest(".specificWorkout"));
     });
   }
   // toggles the options in the select HTML tag to show either elevation(running) or cadence(cycling)
@@ -305,8 +307,7 @@ class App {
         this.marker = null;
       }
     }
-    logWorkout.classList.toggle("hidden");
-    logWorkout.classList.toggle("show");
+    // logWorkout.classList.toggle("hidden");
   }
   _checkInputValues(event) {
     if (event.target.tagName === "INPUT") {
@@ -317,6 +318,7 @@ class App {
   }
   // adds a new workout based on the options selected in the select HTML tag.
   _newWorkout(e) {
+    console.log("enter");
     e.preventDefault();
     let workout;
     const { lat } = this.#mapEvent.latlng;
@@ -343,8 +345,10 @@ class App {
     console.log(workout);
     this.#allWorkouts.push(workout);
     this._addOnHTMLWorkouts(workout.HTML());
-    this._clearInputValues();
     this._addMarker(workout);
+    this._clearInputValues();
+    cadenceInput.blur();
+    elevationGainInput.blur();
     //
   }
   _determineWorkOutInput(type) {
@@ -379,6 +383,10 @@ class App {
       elevationGainInput.value =
       cadenceInput.value =
         "";
+    cadenceInput.blur();
+    elevationGainInput.blur();
+    // logWorkout.classList.toggle("hidden");
+    // this.clickCount++;
   }
   _addOnHTMLWorkouts(html) {
     workoutsContainer.insertAdjacentHTML("afterbegin", html);
