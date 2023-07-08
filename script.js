2a7e417f902bd87e8fc19d950db6fec5b5f52137
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-07-08 02:27:27 -0500

    perfect state and ready

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ const months = [
 // // Create workout objects (Object Oriented Programming)
 
 class Workout {
+  static totalInstances = 0;
   date = new Date();
   id = this.date.getTime() + 1;
   //
@@ -44,6 +45,8 @@ class Workout {
     this.coordinates = coordinates;
     this.duration = this.roundTwoDecimalPlaces(duration);
     this.distance = this.roundTwoDecimalPlaces(distance);
+    this.item = Workout.totalInstances + 1;
+    Workout.totalInstances++;
   }
   roundTwoDecimalPlaces(num) {
     return Number(parseFloat(num).toFixed(2));
@@ -63,6 +66,7 @@ class Running extends Workout {
   }
   HTML() {
     return `<div class="specificWorkout" data-id="${this.id}">
+    <p class="itemNum">#${this.item}<p>
     <svg class="delete" data-id="${
       this.id
     }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
@@ -133,7 +137,7 @@ class Running extends Workout {
   </div>`;
   }
   displayText() {
-    return `${this.type} on ${
+    return `#${this.item}: ${this.type} on ${
       months[this.date.getMonth()]
     } ${this.date.getDay()}`;
   }
@@ -155,6 +159,7 @@ class Cycling extends Workout {
   }
   HTML() {
     return `<div class="specificWorkout" data-id="${this.id}">
+    <p class="itemNum">#${this.item}<p>
     <svg class="delete" data-id="${
       this.id
     }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"/></svg>
@@ -222,7 +227,7 @@ class Cycling extends Workout {
   </div>`;
   }
   displayText() {
-    return `${this.type} on ${
+    return `#${this.item}: ${this.type} on ${
       months[this.date.getMonth()]
     } ${this.date.getDay()}`;
   }
@@ -246,20 +251,22 @@ class App {
     logWorkout.addEventListener("submit", this._newWorkout.bind(this));
     logWorkout.addEventListener("input", this._checkInputValues);
     workoutsContainer.addEventListener("click", function (e) {
-      console.log(e.target.closest(".delete"));
-      console.log(e.target.closest(".specificWorkout"));
+      if (e.target.closest(".delete")) {
+        console.log("delete");
+      }
+      if (e.target.closest(".specificWorkout")) {
+        console.log("delete");
+      }
     });
   }
   // toggles the options in the select HTML tag to show either elevation(running) or cadence(cycling)
   _toggleElevationField() {
     if (selectType.value === "Cycling") {
-      console.log("Cycling");
       elevationGainContainer.style.display = "";
       elevationGainInput.setAttribute("required", "");
       cadenceInput.removeAttribute("required");
       cadenceContainer.style.display = "none";
     } else if (selectType.value === "Running") {
-      console.log("Running");
       cadenceContainer.style.display = "";
       cadenceInput.setAttribute("required", "");
       elevationGainInput.removeAttribute("required");
@@ -306,7 +313,7 @@ class App {
         this.marker = null;
       }
     }
-    // logWorkout.classList.toggle("hidden");
+    logWorkout.classList.toggle("hidden");
   }
   _checkInputValues(event) {
     if (event.target.tagName === "INPUT") {
@@ -318,7 +325,6 @@ class App {
   // adds a new workout based on the options selected in the select HTML tag.
   _newWorkout(e) {
     durationInput.focus();
-    console.log("enter");
     e.preventDefault();
     let workout;
     const { lat } = this.#mapEvent.latlng;
@@ -347,8 +353,6 @@ class App {
     this._addOnHTMLWorkouts(workout.HTML());
     this._addMarker(workout);
     this._clearInputValues();
-    cadenceInput.blur();
-    elevationGainInput.blur();
     //
   }
   _determineWorkOutInput(type) {
